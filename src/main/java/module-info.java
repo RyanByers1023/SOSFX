80911933cd965a4ae8fb822eea418ae6295f39ad
Ryan Byers <ryanbyers1023@gmail.com>
2024-09-06 17:04:27 -0500

    Create project files

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.example.sosfx {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.ikonli.javafx;
+    requires eu.hansolo.tilesfx;
+
+    opens com.example.sosfx to javafx.fxml;
+    exports com.example.sosfx;
+}
